957bee32c2fea22c95b895be858a08c0108a86d7
miriamvisus <miriam.visus.martin@alumnos.upm.es>
2024-02-10 17:54:35 +0100

    Assets

---
+++ BeatJumperUnity/Assets/Scripts/BunnyMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyMovement : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody2D Rigidbody2D;
+    private float Horizontal;
+    private bool Grounded;
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal") * Speed;
+
+        if (Physics2D.Raycast(transform.position, Vector3.down, 2.0f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && Grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+    
+    private void FixedUpdate() 
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
+    }
+
+}
