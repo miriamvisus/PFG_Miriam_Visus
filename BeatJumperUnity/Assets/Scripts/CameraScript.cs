c5cefb4cbb4c2b2043a65e6fa4f8ebfab646f51a
miriamvisus <miriam.visus.martin@alumnos.upm.es>
2024-02-25 13:04:21 +0100

    main menu

---
+++ BeatJumperUnity/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject bunny;
+   
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x = bunny.transform.position.x;
+        transform.position = position;
+    }
+}
