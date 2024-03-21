012320c1c4847624248ef1eb902f04c2e53ac56d
miriamvisus <miriam.visus.martin@alumnos.upm>
2024-03-21 21:31:28 +0100

    conexión

---
+++ BeatJumperUnity/Assets/Scripts/AudioSender.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+public class AudioSender : MonoBehaviour
+{
+    public string serverIP = "127.0.0.1"; 
+    public int port = 8888; 
+
+    void Start()
+    {
+        // Carga el archivo de audio
+        AudioClip audioClip = GetComponent<AudioSource>().clip;
+        float[] audioData = new float[audioClip.samples * audioClip.channels];
+        audioClip.GetData(audioData, 0);
+
+        // Establece la conexión
+        TcpClient client = new TcpClient(serverIP, port);
+        NetworkStream stream = client.GetStream();
+
+        // Envía el tamaño del archivo de audio
+        byte[] sizeBytes = System.BitConverter.GetBytes(audioData.Length);
+        stream.Write(sizeBytes, 0, sizeBytes.Length);
+
+        Debug.Log("Tamaño del archivo de audio enviado: " + audioData.Length + " bytes");
+
+        // Envía los datos de audio
+        int totalBytesSent = 0;
+        foreach (float sample in audioData)
+        {
+            byte[] sampleBytes = System.BitConverter.GetBytes(sample);
+            stream.Write(sampleBytes, 0, sampleBytes.Length);
+            totalBytesSent += sampleBytes.Length;
+        }
+
+        Debug.Log("Total de bytes de audio enviados: " + totalBytesSent + " bytes");
+
+        // Cierra la conexión
+        stream.Close();
+        client.Close();
+    }
+}
