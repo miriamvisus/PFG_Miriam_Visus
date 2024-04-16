dd2c08d4906acda1409ec32932a78830caf2d5d7
miriamvisus <miriam.visus.martin@alumnos.upm>
2024-04-16 19:39:15 +0200

    h

--- BeatJumperPython/process_audio_data.py
+++ BeatJumperPython/process_audio_data.py
@@ -1,7 +1,45 @@
+import socket
 import librosa
 import numpy as np
 import scipy.signal as signal
+import requests
+from io import BytesIO
+import struct
+import sounddevice as sd
+import soundfile as sf
+from pydub import AudioSegment
 
+# Cargar las imágenes desde el repositorio Git
+def load_audios_from_git(git_repo_url, audio_folder, num_audios):
+    audio_data = []
+    base_url = git_repo_url.rstrip('/')  + '/raw/main'
+
+    for i in range(num_audios):
+        audio_path = f"{audio_folder}/Audio{i+1}.mp3"  # Cambiar la extensión según el formato de tus audios
+        audio_url = f"{base_url}/{audio_path}"
+
+        response_audio = requests.get(audio_url)
+
+        if response_audio.status_code == 200:
+            # Convertir el contenido del audio a formato de array NumPy
+            audio_content = BytesIO(response_audio.content)
+            # Cargar el audio utilizando librosa
+            y, sr = librosa.load(audio_content, sr=None)
+            audio_data.append((y, sr))
+        else:
+            print(f"Failed to fetch audio from {audio_url}. Status code: {response_audio.status_code}")
+
+    return audio_data
+
+# Especifica los nombres de las carpetas y la cantidad de imágenes por carpeta en el repositorio Git
+git_repo_url = 'https://github.com/miriamvisus/PFG_Miriam_Visus_Martin'
+audio_folder = 'AUDIOS'
+num_audios = 33
+
+# Carga las imágenes desde el repositorio Git
+audios_data = load_audios_from_git(git_repo_url, audio_folder, num_audios)
+
+#Calcular tempo y energía
 def process_audio_data(audio_file):
     try:
         # Cargar los datos de audio utilizando librosa.load()
@@ -39,8 +77,7 @@ def calculate_tempo(y, sr):
     else:
         return None
 
-
-if __name__ == "__main__":
-    # Procesar los datos de audio
-    audio = "C:\\Users\\miria.PORMIR\\PFG_Miriam_Visus_Martin\\BeatJumperUnity\\Assets\\Audios\\Boogie Party.mp3"
-    tempo, energy = process_audio_data(audio)
+# Procesa cada audio cargado
+for idx, audio_data in enumerate(audios_data):
+    print(f"Procesando audio {idx + 1}")
+    process_audio_data(audio_data)
