1cdf06e85fb570089f840274e15df2e1a66f2b5a
miriamvisus <miriam.visus.martin@alumnos.upm>
2024-04-17 19:47:52 +0200

    b

--- BeatJumperPython/process_audio_data.py
+++ BeatJumperPython/process_audio_data.py
@@ -11,46 +11,6 @@ import sounddevice as sd
 import soundfile as sf
 from pydub import AudioSegment
 
-# Cargar las imágenes desde el repositorio Git
-def load_audios_from_git(git_repo_url, audio_folder, num_audios):
-    tempos = []
-    energias = []
-    base_url = git_repo_url.rstrip('/')  + '/raw/main'
-
-    for i in range(num_audios):
-        audio_path = f"{audio_folder}/Audio{i+1}.mp3"  # Cambiar la extensión según el formato de tus audios
-        audio_url = f"{base_url}/{audio_path}"
-
-        response_audio = requests.get(audio_url)
-
-        if response_audio.status_code == 200:
-            with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as temp_audio_file:
-                temp_audio_file.write(response_audio.content)
-                temp_audio_file.close()
-                audio_file_path = temp_audio_file.name
-
-                try:
-                    # Procesar los datos de audio
-                    print(f"Procesando audio {i + 1}")
-                    tempo, energy = process_audio_data(audio_file_path)
-
-                    # Agregar los datos a las listas acumuladas
-                    tempos.append(tempo)
-                    energias.append(energy)
-
-                finally:
-                    # Eliminar el archivo temporal después de usarlo
-                    os.unlink(audio_file_path)
-        else:
-            print(f"Failed to fetch audio from {audio_url}. Status code: {response_audio.status_code}")
-
-
-# Especifica los nombres de las carpetas y la cantidad de imágenes por carpeta en el repositorio Git
-git_repo_url = 'https://github.com/miriamvisus/PFG_Miriam_Visus_Martin'
-audio_folder = 'AUDIOS'
-num_audios = 33
-
-
 #Calcular tempo y energía
 def process_audio_data(audio_file):
     try:
@@ -88,6 +48,46 @@ def calculate_tempo(y, sr):
         return tempo
     else:
         return None
+
+
+# Cargar las imágenes desde el repositorio Git
+def load_audios_from_git(git_repo_url, audio_folder, num_audios):
+    tempos = []
+    energias = []
+    base_url = git_repo_url.rstrip('/')  + '/raw/main'
+
+    for i in range(num_audios):
+        audio_path = f"{audio_folder}/Audio{i+1}.mp3"  # Cambiar la extensión según el formato de tus audios
+        audio_url = f"{base_url}/{audio_path}"
+
+        response_audio = requests.get(audio_url)
+
+        if response_audio.status_code == 200:
+            with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as temp_audio_file:
+                temp_audio_file.write(response_audio.content)
+                temp_audio_file.close()
+                audio_file_path = temp_audio_file.name
+
+                try:
+                    # Procesar los datos de audio
+                    print(f"Procesando audio {i+1}")
+                    tempo, energy = process_audio_data(audio_file_path)
+
+                    # Agregar los datos a las listas acumuladas
+                    tempos.append(tempo)
+                    energias.append(energy)
+
+                finally:
+                    # Eliminar el archivo temporal después de usarlo
+                    os.unlink(audio_file_path)
+        else:
+            print(f"Failed to fetch audio from {audio_url}. Status code: {response_audio.status_code}")
+
+
+# Especifica los nombres de las carpetas y la cantidad de imágenes por carpeta en el repositorio Git
+git_repo_url = 'https://github.com/miriamvisus/PFG_Miriam_Visus_Martin'
+audio_folder = 'AUDIOS'
+num_audios = 108
 
 
 # Carga los audios desde el repositorio Git
