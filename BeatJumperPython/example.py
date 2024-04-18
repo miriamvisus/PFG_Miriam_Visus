5d5c304d10b83ed16f8befa7c7d7a578971c263e
miriamvisus <miriam.visus.martin@alumnos.upm.es>
2024-04-18 20:45:10 +0200

    pad

--- BeatJumperPython/example.py
+++ BeatJumperPython/example.py
@@ -317,3 +317,132 @@ print(f'Loss on test set: {loss}')
 
 # Guardar el modelo entrenado
 model.save('platform_generation_model.h5')
+
+
+
+import socket
+import librosa
+import numpy as np
+import scipy.signal as signal
+import requests
+import os
+import tempfile
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Conv2D, Dense, Activation, Flatten, Reshape, UpSampling2D, MaxPooling2D, BatchNormalization, Dropout, LeakyReLU, Concatenate
+from tensorflow.keras.optimizers import Adam
+
+from tensorflow.keras.layers import concatenate
+from io import BytesIO
+import struct
+import sounddevice as sd
+import soundfile as sf
+from pydub import AudioSegment
+
+#Calcular tempo y energía
+def process_audio_data(audio_file):
+    try:
+        # Cargar los datos de audio utilizando librosa.load()
+        y, sr = librosa.load(audio_file)
+
+        # Calcular el tempo
+        tempo = calculate_tempo(y, sr)
+
+        # Calcular la energía
+        energy = librosa.feature.rms(y=y)
+
+        print("Tempo:", tempo)
+        print("Energía:", energy)
+
+        return tempo, energy
+
+    except Exception as e:
+        print(f"Error al procesar datos de audio: {e}")
+
+
+def calculate_tempo(y, sr):
+    # Calcular la autocorrelación del audio
+    autocorr = np.correlate(y, y, mode='full')
+
+    # Tomar solo la parte positiva de la autocorrelación (lado derecho del eje)
+    autocorr = autocorr[len(autocorr) // 2:]
+
+    # Encontrar los picos en la autocorrelación
+    peaks, _ = signal.find_peaks(autocorr, height=0)
+
+    # Calcular el tempo en beats por minuto (BPM)
+    if len(peaks) > 1:
+        tempo = sr / (peaks[1] - peaks[0]) * 60
+        return tempo
+    else:
+        return None
+
+
+# Cargar las imágenes desde el repositorio Git
+def load_audios_from_git(git_repo_url, audio_folder, num_audios):
+    tempos = []
+    energias = []
+    base_url = git_repo_url.rstrip('/')  + '/raw/main'
+
+    for i in range(num_audios):
+        audio_path = f"{audio_folder}/Audio{i+1}.mp3"
+        audio_url = f"{base_url}/{audio_path}"
+
+        response_audio = requests.get(audio_url)
+
+        if response_audio.status_code == 200:
+            with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as temp_audio_file:
+                temp_audio_file.write(response_audio.content)
+                temp_audio_file.close()
+                audio_file_path = temp_audio_file.name
+
+                try:
+                    # Procesar los datos de audio
+                    print(f"Procesando audio {i+1}")
+                    tempo, energy = process_audio_data(audio_file_path)
+
+                    # Agregar los datos a las listas acumuladas
+                    tempos.append(np.array(tempo))
+                    energias.append(np.array(energy))
+
+                finally:
+                    # Eliminar el archivo temporal después de usarlo
+                    os.unlink(audio_file_path)
+        else:
+            print(f"Failed to fetch audio from {audio_url}. Status code: {response_audio.status_code}")
+
+    return np.array(tempos), np.array(energias)
+
+
+# Especifica los nombres de las carpetas y la cantidad de imágenes por carpeta en el repositorio Git
+git_repo_url = 'https://github.com/miriamvisus/PFG_Miriam_Visus_Martin'
+audio_folder = 'AUDIOS'
+num_audios = 20
+
+
+# Carga los audios desde el repositorio Git
+tempos, energias = load_audios_from_git(git_repo_url, audio_folder, num_audios)
+
+# Convertir las listas de tempos y energías a matrices numpy para facilitar el manejo
+tempos_array = np.array(tempos)
+energias_array = np.array(energias)
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+tempos_train, tempos_test, energias_train, energias_test = train_test_split(tempos_array, energias_array, test_size=0.2, random_state=42)
+
+# Entrenar modelos individuales para tempo y energía
+tempo_model = LinearRegression()
+energia_model = LinearRegression()
+
+tempo_model.fit(tempos_train.reshape(-1, 1), tempos_train)
+energia_model.fit(energias_train.reshape(-1, 1), energias_train)
+
+# Evaluar los modelos en el conjunto de prueba
+tempo_score = tempo_model.score(tempos_test.reshape(-1, 1), tempos_test)
+energia_score = energia_model.score(energias_test.reshape(-1, 1), energias_test)
+print("Coeficiente de determinación R^2 para el modelo de tempo:", tempo_score)
+print("Coeficiente de determinación R^2 para el modelo de energía:", energia_score)
+
+
+
--- BeatJumperPython/process_audio_data.py
+++ BeatJumperPython/process_audio_data.py
@@ -7,6 +7,11 @@ import os
 import tempfile
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Conv2D, Dense, Activation, Flatten, Reshape, UpSampling2D, MaxPooling2D, BatchNormalization, Dropout, LeakyReLU, Concatenate
+from tensorflow.keras.optimizers import Adam
+
+from tensorflow.keras.layers import concatenate
 from io import BytesIO
 import struct
 import sounddevice as sd
@@ -59,7 +64,7 @@ def load_audios_from_git(git_repo_url, audio_folder, num_audios):
     base_url = git_repo_url.rstrip('/')  + '/raw/main'
 
     for i in range(num_audios):
-        audio_path = f"{audio_folder}/Audio{i+1}.mp3"  # Cambiar la extensión según el formato de tus audios
+        audio_path = f"{audio_folder}/Audio{i+1}.mp3"
         audio_url = f"{base_url}/{audio_path}"
 
         response_audio = requests.get(audio_url)
@@ -76,8 +81,8 @@ def load_audios_from_git(git_repo_url, audio_folder, num_audios):
                     tempo, energy = process_audio_data(audio_file_path)
 
                     # Agregar los datos a las listas acumuladas
-                    tempos.append(tempo)
-                    energias.append(energy)
+                    tempos.append(np.array(tempo))
+                    energias.append(np.array(energy))
 
                 finally:
                     # Eliminar el archivo temporal después de usarlo
@@ -85,18 +90,44 @@ def load_audios_from_git(git_repo_url, audio_folder, num_audios):
         else:
             print(f"Failed to fetch audio from {audio_url}. Status code: {response_audio.status_code}")
 
-    return tempos, energias
+    return np.array(tempos), np.array(energias)
 
 
 # Especifica los nombres de las carpetas y la cantidad de imágenes por carpeta en el repositorio Git
 git_repo_url = 'https://github.com/miriamvisus/PFG_Miriam_Visus_Martin'
 audio_folder = 'AUDIOS'
-num_audios = 86
+num_audios = 20
 
 
 # Carga los audios desde el repositorio Git
 tempos, energias = load_audios_from_git(git_repo_url, audio_folder, num_audios)
 
+# Dividir los datos en conjuntos de entrenamiento y prueba
+tempos_train, tempos_test, energias_train, energias_test = train_test_split(tempos, energias, test_size=0.2, random_state=42)
+
+# Definir la arquitectura del modelo CNN
+model = Sequential([
+    Conv1D(64, 3, activation='relu', input_shape=(None, 1)),
+    MaxPooling1D(2),
+    Conv1D(128, 3, activation='relu'),
+    MaxPooling1D(2),
+    Conv1D(256, 3, activation='relu'),
+    MaxPooling1D(2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(2)  # Dos salidas: tempo y energía
+])
+
+# Compilar el modelo
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Entrenar el modelo
+model.fit(energias_train, tempos_train, epochs=10, batch_size=32, validation_data=(energias_test, tempos_test))
+
+# Evaluar el modelo en el conjunto de prueba
+loss = model.evaluate(energias_test, tempos_test)
+print("Loss:", loss)
 
 
 
