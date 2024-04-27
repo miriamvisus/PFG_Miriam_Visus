a9895ad81e4a8cc17141333077e17aae55e771ee
miriamvisus <miriam.visus.martin@alumnos.upm.es>
2024-04-27 23:22:54 +0200

    a

--- BeatJumperPython/train_model.py
+++ BeatJumperPython/train_model.py
@@ -283,4 +283,4 @@ loss = model.evaluate([tempos_test, energies_test], [character_speeds_test, plat
 print("Pérdida en el conjunto de prueba:", loss)
 
 # Guardar el modelo entreando
-model.save('trained_model.h5')
+model.save('trained_model.keras')
---
+++ BeatJumperUnity/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class AIAgent : Agent
+{
+    
+}
