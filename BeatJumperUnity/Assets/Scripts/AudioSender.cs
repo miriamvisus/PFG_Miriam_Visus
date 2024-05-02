f586cb54fbf0db245b461cf998054216afa68bc1
miriamvisus <miriam.visus.martin@alumnos.upm>
2024-05-02 13:05:20 +0200

    az

--- BeatJumperUnity/Assets/Scripts/AudioSender.cs
+++ BeatJumperUnity/Assets/Scripts/AudioSender.cs
@@ -4,7 +4,6 @@ using System.Net.Sockets;
 using System.IO;
 using UnityEngine;
 
-
 public class AudioSender : MonoBehaviour
 {
     public string serverIP = "127.0.0.1"; 
@@ -12,34 +11,41 @@ public class AudioSender : MonoBehaviour
 
     void Start()
     {
-        // Carga el archivo de audio
-        AudioClip audioClip = GetComponent<AudioSource>().clip;
-        float[] audioData = new float[audioClip.samples * audioClip.channels];
-        audioClip.GetData(audioData, 0);
-
-        // Establece la conexión
-        TcpClient client = new TcpClient(serverIP, port);
-        NetworkStream stream = client.GetStream();
-    
-        // Envía el tamaño del archivo de audio
-        byte[] sizeBytes = System.BitConverter.GetBytes(audioData.Length);
-        stream.Write(sizeBytes, 0, sizeBytes.Length);
-
-        Debug.Log("Tamaño del archivo de audio enviado: " + audioData.Length + " bytes");
-
-        // Envía los datos de audio
-        int totalBytesSent = 0;
-        foreach (float sample in audioData)
+        try
         {
-            byte[] sampleBytes = System.BitConverter.GetBytes(sample);
-            stream.Write(sampleBytes, 0, sampleBytes.Length);
-            totalBytesSent += sampleBytes.Length;
+            // Carga el archivo de audio
+            AudioClip audioClip = GetComponent<AudioSource>().clip;
+            float[] audioData = new float[audioClip.samples * audioClip.channels];
+            audioClip.GetData(audioData, 0);
+
+            // Establece la conexión
+            TcpClient client = new TcpClient(serverIP, port);
+            NetworkStream stream = client.GetStream();
+        
+            // Envía el tamaño del archivo de audio
+            byte[] sizeBytes = System.BitConverter.GetBytes(audioData.Length);
+            stream.Write(sizeBytes, 0, sizeBytes.Length);
+
+            Debug.Log("Tamaño del archivo de audio enviado: " + audioData.Length + " bytes");
+
+            // Envía los datos de audio
+            int totalBytesSent = 0;
+            foreach (float sample in audioData)
+            {
+                byte[] sampleBytes = System.BitConverter.GetBytes(sample);
+                stream.Write(sampleBytes, 0, sampleBytes.Length);
+                totalBytesSent += sampleBytes.Length;
+            }
+
+            Debug.Log("Total de bytes de audio enviados: " + totalBytesSent + " bytes");
+
+            // Cierra la conexión
+            stream.Close();
+            client.Close();
         }
-
-        Debug.Log("Total de bytes de audio enviados: " + totalBytesSent + " bytes");
-
-        // Cierra la conexión
-        stream.Close();
-        client.Close();
+        catch (Exception e)
+        {
+            Debug.LogError("Error al enviar datos de audio: " + e.Message);
+        }
     }
 }
--- BeatJumperUnity/Assets/Scripts/DataReceiver.cs
+++ BeatJumperUnity/Assets/Scripts/DataReceiver.cs
@@ -4,7 +4,6 @@ using System.Net.Sockets;
 using System.IO;
 using UnityEngine;
 
-
 public class DataReceiver : MonoBehaviour
 {
     public string serverIP = "127.0.0.1"; 
@@ -14,34 +13,41 @@ public class DataReceiver : MonoBehaviour
 
     void Start()
     {
-        // Establece la conexión
-        server = new TcpListener(IPAddress.Parse(serverIP), port);
-        server.Start();
-        client = server.AcceptTcpClient();
-        NetworkStream stream = client.GetStream();
+        try
+        {
+            // Establece la conexión
+            server = new TcpListener(IPAddress.Parse(serverIP), port);
+            server.Start();
+            client = server.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
 
-        // Recibe los datos de tempo
-        byte[] tempoBytes = new byte[4];
-        stream.Read(tempoBytes, 0, tempoBytes.Length);
-        float tempo = BitConverter.ToSingle(tempoBytes, 0);
+            // Recibe los datos de tempo
+            byte[] tempoBytes = new byte[4];
+            stream.Read(tempoBytes, 0, tempoBytes.Length);
+            float tempo = BitConverter.ToSingle(tempoBytes, 0);
 
-        // Recibe la longitud del array de energía
-        byte[] energyLengthBytes = new byte[4];
-        stream.Read(energyLengthBytes, 0, energyLengthBytes.Length);
-        int energyLength = BitConverter.ToInt32(energyLengthBytes, 0);
+            // Recibe la longitud del array de energía
+            byte[] energyLengthBytes = new byte[4];
+            stream.Read(energyLengthBytes, 0, energyLengthBytes.Length);
+            int energyLength = BitConverter.ToInt32(energyLengthBytes, 0);
 
-        // Recibe los datos de energía
-        byte[] energyBytes = new byte[sizeof(float) * energyLength];
-        stream.Read(energyBytes, 0, energyBytes.Length);
-        float[] energy = new float[energyLength];
-        Buffer.BlockCopy(energyBytes, 0, energy, 0, energyBytes.Length);
+            // Recibe los datos de energía
+            byte[] energyBytes = new byte[sizeof(float) * energyLength];
+            stream.Read(energyBytes, 0, energyBytes.Length);
+            float[] energy = new float[energyLength];
+            Buffer.BlockCopy(energyBytes, 0, energy, 0, energyBytes.Length);
 
-        // Cierra la conexión
-        stream.Close();
-        client.Close();
+            // Cierra la conexión
+            stream.Close();
+            client.Close();
 
-        // Haz lo que quieras con los datos de tempo y energía
-        Debug.Log("Tempo recibido: " + tempo);
-        Debug.Log("Energía recibida: " + string.Join(", ", energy));
+            // Haz lo que quieras con los datos de tempo y energía
+            Debug.Log("Tempo recibido: " + tempo);
+            Debug.Log("Energía recibida: " + string.Join(", ", energy));
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error al recibir datos de audio: " + e.Message);
+        }
     }
 }
