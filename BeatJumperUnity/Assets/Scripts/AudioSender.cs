13b8722cead0433727f184d3f87f7754614aeea4
miriamvisus <miriam.visus.martin@alumnos.upm.es>
2024-05-03 18:54:57 +0200

    ch

--- BeatJumperUnity/Assets/Scripts/AudioSender.cs
+++ BeatJumperUnity/Assets/Scripts/AudioSender.cs
@@ -15,7 +15,7 @@ public class AudioSender : MonoBehaviour
     {
         try
         {
-            // Carga el archivo de audio
+             // Carga el archivo de audio
             AudioClip audioClip = GetComponent<AudioSource>().clip;
             float[] audioData = new float[audioClip.samples * audioClip.channels];
             audioClip.GetData(audioData, 0);
@@ -23,26 +23,27 @@ public class AudioSender : MonoBehaviour
             // Establece la conexión
             TcpClient client = new TcpClient(serverIP, port);
             NetworkStream stream = client.GetStream();
-            
+
             // Envía el tamaño del archivo de audio
-            byte[] sizeBytes = BitConverter.GetBytes(audioData.Length);
+            byte[] sizeBytes = System.BitConverter.GetBytes(audioData.Length);
             stream.Write(sizeBytes, 0, sizeBytes.Length);
 
             Debug.Log("Tamaño del archivo de audio enviado: " + audioData.Length + " bytes");
 
-            // Envía los datos de audio en búferes de 1024 bytes
-            int bufferSize = 1024;
+            // Envía los datos de audio
             int totalBytesSent = 0;
-            for (int i = 0; i < audioData.Length; i += bufferSize)
+            foreach (float sample in audioData)
             {
-                int bytesToSend = Math.Min(bufferSize, audioData.Length - i);
-                byte[] buffer = new byte[bytesToSend * sizeof(float)];
-                Buffer.BlockCopy(audioData, i * sizeof(float), buffer, 0, bytesToSend * sizeof(float));
-                stream.Write(buffer, 0, buffer.Length);
-                totalBytesSent += buffer.Length;
+                byte[] sampleBytes = System.BitConverter.GetBytes(sample);
+                stream.Write(sampleBytes, 0, sampleBytes.Length);
+                totalBytesSent += sampleBytes.Length;
             }
 
-                Debug.Log("Total de bytes de audio enviados: " + totalBytesSent + " bytes");
+            Debug.Log("Total de bytes de audio enviados: " + totalBytesSent + " bytes");
+
+            // Cierra la conexión
+            stream.Close();
+            client.Close();
         }
         
         catch (Exception ex)
