74a35b6df1c900c44a31008b4ce5f015b0252f2b
miriamvisus <miriam.visus.martin@alumnos.upm.es>
2024-05-09 10:46:59 +0200

    ds

--- BeatJumperUnity/Assets/Scripts/DataReceiver.cs
+++ BeatJumperUnity/Assets/Scripts/DataReceiver.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using PimDeWitte.UnityMainThreadDispatcher;
 
 
 public class DataReceiver : MonoBehaviour
